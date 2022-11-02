444196f0d5853a2b010b58abbf3e6f9c17ed0460
StepanTymofiichuk <75804157+StepanTymofiichuk@users.noreply.github.com>
2022-11-02 17:37:20 +0200

    update resources

---
+++ src/compoonents/resources/description.js
@@ -0,0 +1,32 @@
+export const testDescription = {
+    "tests_description": [
+        {
+            "title": "Progress Test 1",
+            "questions": 40,
+            "tenses": ["Present", "Past", "Future", "the infinitive", "Present", "Enough Participles"],
+            "tags": ["Unit 1", "Unit 2"],
+            "link": "test1.html" 
+        },
+        {
+            "title": "Progress Test 2",
+            "questions": 40,
+            "tenses": ["Adjectives", "Adverbs", "Comparisons", "Nouns", "Articles", "Word Formation"],
+            "tags": ["Unit 3", "Unit 4"],
+            "link": "test2.html" 
+        },
+        {
+            "title": "Progress Test 3",
+            "questions": 40,
+            "tenses": ["Modal Verbs", "The Passive", "Have Something Done"],
+            "tags": ["Unit 5", "Unit 6"],
+            "link": "test3.html" 
+        },
+        {
+            "title": "Progress Test 4",
+            "questions": 40,
+            "tenses": ["Reported Speech", "Emphasis-Inversion"],
+            "tags": ["Unit 7", "Unit 8"],
+            "link": "test4.html" 
+        }
+    ] 
+}
---
+++ src/compoonents/resources/progress_test1.js
@@ -0,0 +1,745 @@
+export const progressTest1 = {
+    "progress_test_1": [
+      {
+        "task": [
+          {
+            "a": "Kate ... hard recently",
+            "b": ""
+          }
+        ],
+        "taskTitle": "Choose the correct answer",
+        "options": [
+          {
+            "text": "has been studying",
+            "correct": true
+          },
+          {
+            "text": "has studied",
+            "correct": false
+          },
+          {
+            "text": "studied",
+            "correct": false
+          }
+        ],
+        "title": "question1"
+      },
+      {
+        "task": [
+          {
+            "a": "I must go to the library.", 
+            "b": "I ... there this afternoon. I'll give you a lift"
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "have been",
+            "correct": false
+          },
+          {
+            "text": "am going",
+            "correct": true
+          },
+          {
+            "text": "go",
+            "correct": false
+          }
+        ],
+        "title":"question2"
+      },
+      {
+        "task": [
+          {
+            "a": "... is very good for you.", 
+            "b": "Yes, it's my favorite form of exercise."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "swim",
+            "correct": false
+          },
+          {
+            "text": "swimming",
+            "correct": true
+          },
+          {
+            "text": "to swim",
+            "correct": false
+          }
+        ],
+        "title":"question3"
+      },
+      {
+        "task": [
+          {
+            "a": "We'd better run to the stadium.", 
+            "b": "I know. the game ... in five minutes."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "is starting",
+            "correct": false
+          },
+          {
+            "text": "has started",
+            "correct": false
+          },
+          {
+            "text": "starts",
+            "correct": true
+          }
+        ],
+        "title":"question4"
+      },
+      {
+        "task": [
+          {
+            "a": "I'm very hungry.", 
+            "b": "I'm not suprised. You ... all day."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "haven't been eating",
+            "correct": false
+          },
+          {
+            "text": "haven't eaten",
+            "correct": true
+          },
+          {
+            "text": "don't eat",
+            "correct": false
+          }
+        ],
+        "title":"question5"
+      },
+      {
+        "task": [
+          {
+            "a": "I can't decide what ... for lunch.", 
+            "b": "Why don't you buy a sandwhich?"
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "have",
+            "correct": false
+          },
+          {
+            "text": "having",
+            "correct": false
+          },
+          {
+            "text": "to have",
+            "correct": true
+          }
+        ],
+        "title":"question6"
+      },
+      {
+        "task": [
+          {
+            "a": "Why did you go to the shop this morning?", 
+            "b": "... some bread. We had run out."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "Get",
+            "correct": false
+          },
+          {
+            "text": "Getting",
+            "correct": false
+          },
+          {
+            "text": "To get",
+            "correct": true
+          }
+        ],
+        "title":"question7"
+      },
+      {
+        "task": [
+          {
+            "a": "How did you hurt your hand?", 
+            "b": "I cut it as I was ... some vegetables"
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "chopped",
+            "correct": false
+          },
+          {
+            "text": "am chopping",
+            "correct": false
+          },
+          {
+            "text": "was chopping",
+            "correct": true
+          }
+        ],
+        "title":"question8"
+      },
+      {
+        "task": [
+          {
+            "a": "The journey here was very tiring.", 
+            "b": "Yes. I'm very glad ... here at last."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "being",
+            "correct": false
+          },
+          {
+            "text": "to be",
+            "correct": true
+          },
+          {
+            "text": "be",
+            "correct": false
+          }
+        ],
+        "title":"question9"
+      },
+      {
+        "task": [
+          {
+            "a": "Let's go for a walk.", 
+            "b": "We can't go out until the rain ..."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "stopped",
+            "correct": false
+          },
+          {
+            "text": "will stop",
+            "correct": false
+          },
+          {
+            "text": "stops",
+            "correct": true
+          }
+        ],
+        "title":"question10"
+      },
+      {
+        "task": [
+          {
+            "a": "I want to call Simon.", 
+            "b": "Well, don't call him untill eight o'clock. He ..."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "will sleep",
+            "correct": false
+          },
+          {
+            "text": "will be sleeping",
+            "correct": true
+          },
+          {
+            "text": "is sleeping",
+            "correct": false
+          }
+        ],
+        "title":"question11"
+      },
+      {
+        "task": [
+          {
+            "a": "Would you like to watch the film with me?", 
+            "b": "No thanks. I'd rather ... my book."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "read",
+            "correct": true
+          },
+          {
+            "text": "reading",
+            "correct": false
+          },
+          {
+            "text": "to read",
+            "correct": false
+          }
+        ],
+        "title":"question12"
+      },
+      {
+        "task": [
+          {
+            "a": "He ... a famous writer one day.", 
+            "b": "Yes, I think you're right."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "was",
+            "correct": false
+          },
+          {
+            "text": "will be",
+            "correct": true
+          },
+          {
+            "text": "is beeing",
+            "correct": false
+          }
+        ],
+        "title":"question13"
+      },
+      {
+        "task": [
+          {
+            "a": "What are you doing on Saturday?", 
+            "b": "Well, Paul suggested ... to the cinema."
+          }
+        ],
+        "taskTitle":"Choose the correct answer",
+        "options": [
+          {
+            "text": "go",
+            "correct": false
+          },
+          {
+            "text": "going",
+            "correct": true
+          },
+          {
+            "text": "to go",
+            "correct": false
+          }
+        ],
+        "title":"question14"
+      },
+      {
+        "task": "This soup is too cold for me to eat.",
+        "word": "enough", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "isn't hot enough",
+        "answer1": "is not hot enough",
+        "beginning": "This soup",
+        "end": "for me to eat.",
+        "title":"question15"
+      },
+      {
+        "task": "We haven't been to the theatre for months.",
+        "word": "time", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "last time we went",
+        "answer1": "the last time we went",
+        "beginning": "The",
+        "end": "to the theatre was month ago.",
+        "title":"question16"
+      },
+      {
+        "task": "The car was so expensive that he couldn't afford it.",
+        "word": "too", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "too expensive for him",
+        "answer1": "too expensive for him",
+        "beginning": "The car",
+        "end": "to afford.",
+        "title":"question17"
+      },
+      {
+        "task": "I haven't written to Paul yet.",
+        "word": "still", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "still haven't written",
+        "answer1": "still haven't written",
+        "beginning": "I",
+        "end": "to Paul.",
+        "title":"question18"
+      },
+      {
+        "task": "Someone saw him leave the building.",
+        "word": "was", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "was seen leaving",
+        "answer1": "was leaving",
+        "beginning": "He",
+        "end": "the building.",
+        "title":"question19"
+      },
+      {
+        "task": "Food is not allowed to be eaten in the library.",
+        "word": "eat", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "are not allowed to eat",
+        "answer1": "are not allowed to eat",
+        "beginning": "People",
+        "end": "in the library",
+        "title":"question20"
+      },
+      {
+        "task": "He was bored by the film and fell asleep.",
+        "word": "found", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "found the film boring",
+        "answer1": "found the film boring",
+        "beginning": "He",
+        "end": "and fell asleep.",
+        "title":"question21"
+      },
+      {
+        "task": "She began playing the piano when she was eight.",
+        "word": "since", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "begun playing the piano since",
+        "answer1": "been playing the piano since",
+        "beginning": "She has",
+        "end": "was eight.",
+        "title":"question22"
+      },
+      {
+        "task": "Telling lies is very bad.",
+        "word": "to", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "is very bad to",
+        "answer1": "is bad to",
+        "beginning": "It",
+        "end": "lies.",
+        "title":"question23"
+      },
+      {
+        "task": "She didn't go to bed until she had finished her homework.",
+        "word": "before", 
+        "taskTitle":"Complete each sentence with two to five words, including the word in italics.",
+        "answer": "her homework before going",
+        "answer1": "her homework before she went",
+        "beginning": "She finished",
+        "end": "to bed.",
+        "title":"question24"
+      },
+      {
+        "task": "She was unwilling ...",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "to help",
+            "correct": true
+          },
+          {
+            "text": "helping",
+            "correct": false
+          },
+          {
+            "text": "help",
+            "correct": false
+          }
+        ],
+        "title": "question25"
+      },
+      {
+        "task": "Tom had difficulty ... the lid",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "to unscrew",
+            "correct": false
+          },
+          {
+            "text": "unscrew",
+            "correct": false
+          },
+          {
+            "text": "unscrewing",
+            "correct": true
+          }
+        ],
+        "title": "question26"
+      },
+      {
+        "task": "I would rather ... Chineese food tonight.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "eating",
+            "correct": false
+          },
+          {
+            "text": "eat",
+            "correct": true
+          },
+          {
+            "text": "to eat",
+            "correct": false
+          }
+        ],
+        "title": "question27"
+      },
+      {
+        "task": "Mary stopped ... a newspaper on her way to work.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "buy",
+            "correct": false
+          },
+          {
+            "text": "buying",
+            "correct": false
+          },
+          {
+            "text": "to buy",
+            "correct": true
+          }
+        ],
+        "title": "question28"
+      },
+      {
+        "task": "He admitted to ... to the police.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "lie",
+            "correct": false
+          },
+          {
+            "text": "lying",
+            "correct": true
+          },
+          {
+            "text": "to lie",
+            "correct": false
+          }
+        ],
+        "title": "question29"
+      },
+      {
+        "task": "They called the theatre box office only ... that there were no tickets left.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "to find",
+            "correct": true
+          },
+          {
+            "text": "finding",
+            "correct": false
+          },
+          {
+            "text": "find",
+            "correct": false
+          }
+        ],
+        "title": "question30"
+      },
+      {
+        "task": "She is too old ... windsurfing.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "to take up",
+            "correct": true
+          },
+          {
+            "text": "taking up",
+            "correct": false
+          },
+          {
+            "text": "take up",
+            "correct": false
+          }
+        ],
+        "title": "question31"
+      },
+      {
+        "task": "After ... his degree, he looked for a job.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "to take",
+            "correct": false
+          },
+          {
+            "text": "taking",
+            "correct": true
+          },
+          {
+            "text": "take",
+            "correct": false
+          }
+        ],
+        "title": "question32"
+      },
+      {
+        "task": "They tried ... the company president, but he was on holiday.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "to locate",
+            "correct": true
+          },
+          {
+            "text": "locate",
+            "correct": false
+          },
+          {
+            "text": "locating",
+            "correct": false
+          }
+        ],
+        "title": "question33"
+      },
+      {
+        "task": "Did he mention anything about ...?",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "leave",
+            "correct": false
+          },
+          {
+            "text": "leaving",
+            "correct": true
+          },
+          {
+            "text": "to leave",
+            "correct": false
+          }
+        ],
+        "title": "question34"
+      },
+      {
+        "task": "I’m sorry ... ask you to do this, but could you possibly work late tonight?",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "have to",
+            "correct": false
+          },
+          {
+            "text": "having",
+            "correct": false
+          },
+          {
+            "text": "to have to",
+            "correct": true
+          }
+        ],
+        "title": "question35"
+      },
+      {
+        "task": "It was kind of you ... me flowers.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "send",
+            "correct": false
+          },
+          {
+            "text": "to send",
+            "correct": true
+          },
+          {
+            "text": "sending",
+            "correct": false
+          }
+        ],
+        "title": "question36"
+      },
+      {
+        "task": "In addition to ... the bus, she also lost her umbrella.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "missing",
+            "correct": true
+          },
+          {
+            "text": "to miss",
+            "correct": false
+          },
+          {
+            "text": "miss",
+            "correct": false
+          }
+        ],
+        "title": "question37"
+      },
+      {
+        "task": "Did you remember ... Aunt Hilary for her present?",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "thank",
+            "correct": false
+          },
+          {
+            "text": "thanking",
+            "correct": false
+          },
+          {
+            "text": "to thank",
+            "correct": true
+          }
+        ],
+        "title": "question38"
+      },
+      {
+        "task": "You shouldn’t ... everything he says.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "believe",
+            "correct": true
+          },
+          {
+            "text": "believing",
+            "correct": false
+          },
+          {
+            "text": "to believe",
+            "correct": false
+          }
+        ],
+        "title": "question39"
+      },
+      {
+        "task": "He wasted valuable time ... on the phone.",
+        "taskTitle": "Put the verbs in brackets into the correct infinitive or -ing form",
+        "options": [
+          {
+            "text": "talk",
+            "correct": false
+          },
+          {
+            "text": "to talk",
+            "correct": false
+          },
+          {
+            "text": "taliking",
+            "correct": true
+          }
+        ],
+        "title": "question40"
+      }
+    ],
+    "taskTitle":"Choose the correct answer"
+  }
